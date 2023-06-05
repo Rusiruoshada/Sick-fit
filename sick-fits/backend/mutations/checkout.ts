711033f8b35079f183e47434b54d6bfac99682ce
roms <rusirusamaraweera749@gmail.com>
2023-06-05 12:14:39 +0530

    setup backend with stripe version 2

--- sick-fits/backend/mutations/checkout.ts
+++ sick-fits/backend/mutations/checkout.ts
@@ -1,15 +1,67 @@
 /* eslint-disable */
 import { KeystoneContext } from '@keystone-next/types';
-import { OrderCreateInput } from '../.keystone/schema-types';
+import { CartItemCreateInput, OrderCreateInput } from '../.keystone/schema-types';
+import stripeConfig from '../lib/stripe';
 
+const graphql = String.raw;
 interface Arguments {
   token: string
 }
 
 async function checkout(
   root: any,
-  { token }:Arguments,
+  { token }: Arguments,
   context: KeystoneContext
-): Promise<OrderCreateInput> {}
+): Promise<OrderCreateInput> {
+  // 1. make sure they are sign in
+  const userId = context.session.itemId;
+  if(!userId){
+    throw new Error('Sorry! You must signed in to create an order')
+  }
+  // 2. Query the current user
+  const user = await context.lists.User.findOne({
+    where: { id: userId },
+    resolveFields: graphql`
+    id
+    name
+    email
+    cart {
+      id
+      quantity
+      product {
+        name
+        price
+        description
+        id
+        photo {
+          id
+          image {
+            id
+            publicUrlTransformed
+          }
+        }
+      }
+    }
+    `
+  });
+
+  // 3. code the total price for their order
+  const cartItems = user.cart.filter( cartItem => cartItem.product);
+  const amount = cartItems.reduce(function(tally: number, cartItem:CartItemCreateInput){
+    return tally + cartItem.quantity * cartItem.product.price;
+  },0);
+
+ // 4. create the payment with the stripe library
+ const charge = await stripeConfig.paymentIntents.create({
+  amount,
+  currency: 'USD',
+  confirm: true,
+  payment_method: token,
+ }).catch(err => {
+  console.log(err);
+  throw new Error(err.message);
+ });
+ console.log(charge);
+}
 
 export default checkout;
